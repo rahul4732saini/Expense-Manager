bf1e8c3f7167c457486e2bbe37f8836b7be2bf28
rahul4732saini <rahulksaini4732@gmail.com>
2023-05-11 15:40:44 +0530

    update

---
+++ transactions/sort.py
@@ -0,0 +1,12 @@
+from sys import path
+path.append("..\\Expense Manager")
+
+from transactions.details import manage
+from transactions.catagory import income, expense
+
+class transactions:
+    def __init__(self):
+        ...
+
+    def sort(self) -> list:
+        ...
