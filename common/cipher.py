9fdd625c3d255073a12496d34785aea746e9bff1
rahul4732saini <rahulksaini4732@gmail.com>
2023-05-12 17:27:50 +0530

    update

---
+++ common/cipher.py
@@ -0,0 +1,9 @@
+class code:
+    def __init__(self, target: str):
+        self.target = target
+
+    def encrypt(self) -> str:
+        ...
+
+    def decrypt(self) -> str:
+        ...
--- transactions/payment_mode.py
+++ transactions/payment_mode.py
@@ -1,11 +1,15 @@
-from sys import path
-path.append("..\\Expense Manager")
+try:
+    from sys import path
+    path.append("..\\Expense Manager")
 
-import os.path
-import data.info as info
-from typing import Union
-from random import choice
-from transactions.details import manage
+    import random
+    import os.path
+    import data.info as info
+    import data.pre_requisites as pre_requisites
+    from typing import Union
+    from transactions.details import manage
+except Exception:
+    raise Exception("0xegbl0001")
 
 class manage:
     def get_modes(self) -> list:
@@ -18,19 +22,68 @@ class manage:
         except Exception:
             raise Exception()
         
+        payment_modes = payment_modes[:30] if payment_modes.__len__() > 30 else payment_modes
+
+        for i in payment_modes:
+            self._verify(i)
+        
         return payment_modes
     
-    def _verify(self) -> None:
-        ...
+    def get_mode_names(self) -> list:
+        return [i.get("name") for i in self.get_modes()]
 
-    def _write_file(self) -> None:
-        ...
+    def _verify(self, payment_modes: dict) -> None:
+        modes = payment_modes
 
-    def add_mode(self) -> None:
-        ...
+        if all(
+            [
+                modes.get("name").__class__ == str,
+                modes.get("catagory") in pre_requisites.PAYMENT_MODE_CATAGORIES,
+                payment_modes.get("current_balance").__class__ in [int, float] and modes.get("current_balance") > 0,
+                modes.get("color") in pre_requisites.COLORS
+            ]
+        ) == False:
+            raise Exception()
+        
+        if modes.get("name").__len__() > 25:
+            raise Exception()
 
-    def delete_mode(self) -> None:
-        ...
+    def _write_file(self, entry: list) -> None:
+        for i in entry:
+            self._verify(i)
+
+        payment_modes: list = self.get_modes()
+        payment_modes.append(entry)
+        
+        payment_modes = str(payment_modes).replace("{", "{\n").replace("}", "\n}").replace("[", "[\n").replace("]", "\n]").replace(", ", ",\n")
+
+        with open(info.DATA_PAYMENT_MODES, 'w') as file:
+            file.write(payment_modes)
+
+    def add_mode(self,
+                 name: str,
+                 catagory: str,
+                 current_balance: Union[int, float] = 0) -> None:
+        
+        color = random.choice(pre_requisites.COLORS)
+
+        entry = {
+            "name": name,
+            "color": color,
+            "current_balance": current_balance,
+            "catagory": catagory
+        }
+
+        self._write_file([entry])
+
+    def delete_mode(self, mode_name: str) -> None:
+        if mode_name not in self.get_mode_names():
+            raise Exception()
+
+        payment_modes = [i for i in self.get_modes() if i.get("name") != mode_name]
+        self._write_file(payment_modes)
 
     def edit_mode(self) -> None:
         ...
+
+manage().delete_mode("UPI")
