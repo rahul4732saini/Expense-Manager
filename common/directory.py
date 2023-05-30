19f097f56b19ce1d0ee2584f3d9804cae211f031
rahul4732saini <rahulksaini4732@gmail.com>
2023-05-30 12:57:21 +0530

    update

--- common/directory.py
+++ common/directory.py
@@ -8,8 +8,7 @@ directory contents.
 -   get_subdirs: returns a list of all sub-directories in the directory path provided.
 """
 
-from typing import Any
-from os import path, listdir
+import os
 
 class Indexer:
     def __init__(self,dir_path:str):
@@ -18,14 +17,14 @@ class Indexer:
     def __repr__(self):
         return f"dir_path = {self.dir_path}"
 
-    def __setattr__(self, name: str, value: Any):
-        if name == "dir_path" and not path.exists(value):
+    def __setattr__(self, name: str, value: object):
+        if name == "dir_path" and not os.path.exists(value):
             raise Exception("0xedir0001")
         
         return super().__setattr__(name, value)
     
-    def get_files(self) -> list:
-        return [i for i in listdir(self.dir_path) if path.isfile("%s\\%s" % (self.dir_path, i))]
+    def get_files(self) -> list[str]:
+        return [i for i in os.listdir(self.dir_path) if os.path.isfile("%s\\%s" % (self.dir_path, i))]
     
-    def get_subdirs(self) -> list:
-        return [i for i in listdir(self.dir_path) if path.isdir("%s\\%s" % (self.dir_path, i))]
+    def get_subdirs(self) -> list[str]:
+        return [i for i in os.listdir(self.dir_path) if os.path.isdir("%s\\%s" % (self.dir_path, i))]
